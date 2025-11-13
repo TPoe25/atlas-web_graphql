a4e2c4a5b31206179bf9d208c03b049e92732afc
Taylor Poe <t_poe25@yahoo.com>
2025-11-13 11:56:00 -0600

    making changes to ProjectType

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -26,7 +26,23 @@ let dummyData2 = {
     description: "Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order"
 };
 
+let dummyData3 = {
+    id: "1",
+    title: "Advanced HTML",
+    weight: 1,
+    description: "Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!"
+};
+
+let dummyData4 = {
+    id: "1",
+    title: "Bootstrap",
+    weight: 1,
+    description: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components."
+};
+
+
 let tasks = [dummyData1, dummyData2];
+let project = [dummyData3, dummyData4];
 
 const TaskType = new GraphQLObjectType({
     name: 'task',
@@ -56,10 +72,19 @@ const RootQuery = new GraphQLObjectType({
 const ProjectType = new GraphQLObjectType({ // ProjectType is a new GraphQLObjectType
     name: 'Project',
     fields: {
-        id: { type: { type: GraphQLID } },
+        id: { type: GraphQLID },
         title: { type: GraphQLString },
         weight: { type: GraphQLInt },
-        description: { type: GraphQLString }
+        description: { type: GraphQLString },
+        project: {
+            type: TaskType,
+            args:  { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                const findProject = lodash.find(project, project => project.id === args.id);
+                // Logic to get data from db / other source
+                return findProject;
+                }
+            }
         }
     }
 );
